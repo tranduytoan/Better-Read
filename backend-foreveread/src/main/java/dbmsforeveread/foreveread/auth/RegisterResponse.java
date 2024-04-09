f4daecaf7984080e6cb1f5ae4a15ac8aaa8f9beb
Tran Duy Toan <toantranx17ht@gmail.com>
2024-04-09 14:30:22 +0700

    Replace LoginResponse and RegisterResponse with TokenDTO (response returns TokenDTO)
    This commit replaces the usage of LoginResponse and RegisterResponse with TokenDTO in the response. Now, the responses return TokenDTO instead of LoginResponse or RegisterResponse.

--- backend-foreveread/src/main/java/dbmsforeveread/foreveread/auth/RegisterResponse.java
+++ backend-foreveread/src/main/java/dbmsforeveread/foreveread/auth/RegisterResponse.java
@@ -1,14 +1,14 @@
-package dbmsforeveread.foreveread.auth;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class RegisterResponse {
-    private String token;
-    private Long userId;
-    private String email;
-}
+//package dbmsforeveread.foreveread.auth;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class RegisterResponse {
+//    private String token;
+//    private Long userId;
+//    private String email;
+//}
